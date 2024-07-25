1fd973deb0a8b01de44a2183536bc1da6d379f6c
Alex Markham <alex.markham@causal.dev>
2024-07-25 07:06:51 +0200

    join_time.csv debugged and tested

--- scripts/rule_scripts/join_time.py
+++ scripts/rule_scripts/join_time.py
@@ -2,31 +2,27 @@ from os import path
 import pandas as pd
 
 # input
-cslearn_pc_path = snakemake.input[0]
-cslearn_grasp_path = snakemake.input[1]
+cslearn_pc_paths = snakemake.input["cslearn_pc"]
+cslearn_grasp_paths = snakemake.input["cslearn_grasp"]
 # bos_path = snakemake.input[2]
 # grasp_bhc_path = snakemake.input[3]
-# p = int(snakemake.wildcards["cstree_p"])
 
 # load times and aux
-with open(cslearn_pc_path, "r") as f:
-    pc_time = f.read()
-with open(cslearn_grasp_path, "r") as f:
-    grasp_time = f.read()
+algs_dict = {"cslearn+pc": cslearn_pc_paths, "cslearn+grasp": cslearn_grasp_paths}
+joined_dict = {"alg": [], "p": [], "n": [], "time": []}
+for alg, paths in algs_dict.items():
+    for path in paths:
+        with open(path, "r") as f:
+            time = f.read()
+        slashed = path.split("/")
+        p = slashed[3].split("=")[-1]
+        n = slashed[13].split("=")[-1]
 
-slashed = cslearn_pc_path.split("/")
-p = slashed[3].split("=")[-1]
-n = slashed[13].split("=")[-1]
-
-# print(cslearn_pc_path)
+        joined_dict["alg"] += [alg]
+        joined_dict["p"] += [p]
+        joined_dict["n"] += [n]
+        joined_dict["time"] += [time]
+times_df = pd.DataFrame(joined_dict)
 
 # output
-# if not path.exists(snakemake.output[0]):
-#     with open(snakemake.output[0], "w") as f:
-#         f.write("alg,p,n,time")
-
-with open(snakemake.output[0], "w") as f:
-    f.write(f"cslearn+pc,{p},{n},{pc_time}\n")
-    f.write(f"cslearn+grasp,{p},{n},{grasp_time}\n")
-
-# print(f"cslearn+pc,{p},{n},{pc_time}\n")
+times_df.to_csv(snakemake.output[0], index=False)
