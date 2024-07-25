6e63f97825486fbc6236a6cb12e880d02499ca21
Felix Rios <felix.leopoldo.rios@gmail.com>
2024-07-25 11:10:55 +0200

    more files

---
+++ scripts/snake_scripts/joint_stagedtree_distr.R
@@ -0,0 +1,23 @@
+ # Generate all the outcomes in the order specified by the data labels
+
+datafile <- snakemake@input$data
+modelfile <- snakemake@input$model
+
+# read data from csv
+data <- read.csv(datafile, header = TRUE, check.names = FALSE)
+# read the model from the rds file
+model <- readRDS(modelfile)
+
+
+spaces <- list()
+i <- 1
+for (c in cardinalities) {
+    spaces[[colnames(data)[[i]]]] <- seq(c) - 1
+    i <- i + 1
+}
+space <- rev(expand.grid(rev(spaces)))
+
+## generate the full sample space
+prob <- prob(model, space)
+log_prob <- prob(model, space, log = TRUE)
+df <- cbind(space, prob = prob, log_prob = log_prob)
