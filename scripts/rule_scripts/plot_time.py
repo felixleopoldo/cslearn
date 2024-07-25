1f445b786a65a8cba0e78074804d21cc53fd521d
Alex Markham <alex.markham@causal.dev>
2024-07-25 15:54:18 +0200

    test time plot produced!

---
+++ scripts/rule_scripts/plot_time.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import seaborn as sns
+
+
+# input
+plot_data_path = snakemake.input[0]
+
+# make plot
+plot_data = pd.read_csv(plot_data_path)
+plot_data["hue"] = plot_data["alg"] + ", n=" + plot_data["n"].astype(str)
+sns.set(font_scale=1.25)
+sns.set_style("white")
+sns.set_style({"legend.frameon": False})
+g = sns.boxplot(data=plot_data, x="p", y="time", hue="alg", style="n")
+
+g.set(
+    title=f"",
+    xlabel="number of variables ($p$)",
+    ylabel="runtime (seconds)",
+)
+
+g.legend_.set_title("")
+g.legend_.set_frame_on(False)
+# new_labels = [
+#     "constraint-based phase (PC)",
+#     "Gibbs sampler and exact search",
+#     "total",
+# ]
+# for t, l in zip(g.legend_.texts, new_labels):
+#     t.set_text(l)
+
+g.figure.tight_layout()
+
+# output
+g.figure.savefig(snakemake.output[0])
