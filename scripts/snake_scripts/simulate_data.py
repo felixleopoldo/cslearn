13861b2d5a36dbc2f2381ee8dd7ffe480d482d07
Felix Rios <felix.leopoldo.rios@gmail.com>
2024-07-30 03:13:16 +0200

    Adding the other time plots. Cslearn seems to be slow reading p=50 size trees.

--- scripts/snake_scripts/simulate_data.py
+++ scripts/snake_scripts/simulate_data.py
@@ -10,13 +10,18 @@ seed = int(snakemake.wildcards["seed"])
 
 # simulate data
 nprandom.seed(seed)
+
 df = read_csv(input_path, index_col=(0))
+print(df)
+print("Reading cstree")
 tree = ct.df_to_cstree(df)
 
-
+print("Sampling data")
 data = tree.sample(n_samples)
+print("Data sampled. Ensuring all columns have at least 2 unique values")
 # make sure all columns are binary otherwise resample the data
 while not all([data[col][1:].nunique() >= 2 for col in data.columns]):
+    print("Resampling data until all columns have at least 2 unique values")
     data = tree.sample(n_samples)
 
 
