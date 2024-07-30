683b1ee8c2753e61d9884e0f2ece89003077ef53
Felix Rios <felix.leopoldo.rios@gmail.com>
2024-07-30 16:14:37 +0200

    Fixing time plot a bit

--- scripts/rule_scripts/join_time.py
+++ scripts/rule_scripts/join_time.py
@@ -3,16 +3,21 @@ from itertools import chain
 
 import pandas as pd
 
+print(snakemake.input.keys())
 # input
-csl_pc_paths = snakemake.input["cslearn_pc"]
-csl_grasp_paths = snakemake.input["cslearn_grasp"]
-bos_paths = snakemake.input["bos"]
-grasp_bhc_path = snakemake.input["grasp_bhc"]
+# csl_pc_paths = snakemake.input["cslearn_pc"]
+# csl_grasp_paths = snakemake.input["cslearn_grasp"]
+# bos_paths = snakemake.input["bos"]
+# grasp_bhc_path = snakemake.input["grasp_bhc"]
 
 # load results
 results = []
-for path in chain(csl_pc_paths, csl_grasp_paths, bos_paths, grasp_bhc_path):
-    results.append(pd.read_csv(path))
+for key, value in snakemake.input.items():
+        for path in value:
+            results.append(pd.read_csv(path))
+
+#for path in chain(csl_pc_paths, csl_grasp_paths, bos_paths, grasp_bhc_path):
+#    results.append(pd.read_csv(path))
 times_df = pd.concat(results)
 
 # output
