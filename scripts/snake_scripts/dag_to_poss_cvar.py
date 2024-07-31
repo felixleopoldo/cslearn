a3dc1e50b6b7ebbbf0bf547b6fed4e702a352b5d
Alex Markham <alex.markham@causal.dev>
2024-07-31 18:52:38 +0200

    fix poss_cvars label bug

--- scripts/snake_scripts/dag_to_poss_cvar.py
+++ scripts/snake_scripts/dag_to_poss_cvar.py
@@ -1,17 +1,18 @@
 import json
 
-from numpy import flatnonzero, loadtxt
+from numpy import loadtxt
 
 # input
 dag_path = snakemake.input[0]
 
 # load dag and find parents (poss_cvars)
 # read dag_path as csv file
-dag = loadtxt(dag_path, delimiter=",")
-header = dag[0]
+dag = loadtxt(dag_path, delimiter=",").astype(int)
+labels = dag[0]
 conv = lambda nparray: [str(i) for i in nparray]
 poss_cvars = {
-    str(idx): conv(flatnonzero(pa_mask)) for idx, pa_mask in enumerate(dag[1:].T)
+    str(label): conv(labels[pa_mask.astype(bool)])
+    for label, pa_mask in zip(labels, dag[1:].T)
 }
 
 # output
