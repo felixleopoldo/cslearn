053380b2fbfcc9ee3ebd548b1d496fb724d3d3cc
Felix Rios <felix.leopoldo.rios@gmail.com>
2024-07-31 02:39:19 +0200

    Pgmpy nodes has to be strings. So parameter estimation seems to work now.

--- scripts/snake_scripts/dag_bdeu_parameter_estimation.py
+++ scripts/snake_scripts/dag_bdeu_parameter_estimation.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import pandas as pd
 from pgmpy.models import BayesianNetwork
+from pgmpy.estimators import MaximumLikelihoodEstimator
 
 # input
 
@@ -11,6 +12,9 @@ print(dag_adjmat.values)
 
 print("read dag")
 dag = nx.from_numpy_array(dag_adjmat.values, create_using=nx.DiGraph)
+
+# Relabel the nodes to strings. Needed for pgmpy!
+dag = nx.relabel_nodes(dag, {i: str(i) for i in range(dag_adjmat.shape[0])})
 
 # print edges
 print("edges:")
@@ -24,7 +28,26 @@ data = pd.read_csv(snakemake.input[1])
 
 print("create pgmpy bn")
 pgm = BayesianNetwork(dag)
-pgm.fit(data[1:])
+print("fit pgmpy bn")
+print(pgm)
+print(pgm.edges)
+print("fit parameters")
+print(data[1:])
 
+# using https://pgmpy.org/param_estimator/mle.html
+# https://pgmpy.org/models/bayesiannetwork.html#module-pgmpy.models.BayesianNetwork
+# an alternative would be causalnex
+# https://github.com/felixleopoldo/cancer_challenge/blob/814c03a210d4533062915e656ae92284db81acc9/challenge2/tozip/code/docalc.ipynb
+
+state_names = {col: [0, 1] for col in data.columns}
+#estimator = MaximumLikelihoodEstimator(pgm, data[1:], state_names=state_names)
+
+
+#estimator.estimate_cpd(0)
+pgm.fit(data[1:])
+print(pgm.get_cpds())
+print("check")
+print(pgm.check_model())
+#estimator.get_parameters()
 # output
 pgm.save(snakemake.output[0])
