21ececf759d34a649ad940fd41c2aadd81099594
Alex Markham <alex.markham@causal.dev>
2024-07-31 13:56:34 +0200

    debugging cpdags

--- scripts/snake_scripts/dag_bdeu_parameter_estimation.py
+++ scripts/snake_scripts/dag_bdeu_parameter_estimation.py
@@ -4,25 +4,28 @@ from pgmpy.models import BayesianNetwork
 from pgmpy.estimators import MaximumLikelihoodEstimator
 
 # input
-dag_adjmat = pd.read_csv(snakemake.input[0], header=[0])
-dag = nx.from_numpy_array(dag_adjmat.values, create_using=nx.DiGraph)
+dag_df = pd.read_csv(snakemake.input[0], header=[0])
+dag = nx.from_numpy_array(dag_df.values, create_using=nx.DiGraph)
 
-# Relabel the nodes to strings. Needed for pgmpy!
-dag = nx.relabel_nodes(dag, {i: str(i) for i in range(dag_adjmat.shape[0])})
+print(f"\nis a DAG: {nx.is_directed_acyclic_graph(dag)}; has adj_mat:\n{dag_df}\n")
+
+# Relabel the nodes to strings (and the correct labels). Needed for pgmpy!
+dag = nx.relabel_nodes(
+    dag, {idx: str(label) for idx, label in enumerate(dag_df.columns)}
+)
 
 data = pd.read_csv(snakemake.input[1])
 # find MLE
 
-
 pgm = BayesianNetwork(dag)
-#print("fit pgmpy bn")
-#print(pgm)
-#print(pgm.edges)
+# print("fit pgmpy bn")
+# print(pgm)
+# print(pgm.edges)
 
 # using https://pgmpy.org/param_estimator/mle.html
 # https://pgmpy.org/models/bayesiannetwork.html#module-pgmpy.models.BayesianNetwork
 
-state_names = {col: [0, 1] for col in data.columns}
+# state_names = {col: [0, 1] for col in data.columns}
 # estimator = MaximumLikelihoodEstimator(pgm, data[1:], state_names=state_names)
 
 
